8680aa804fa3c1fd40610f467e5a71870e112ed5
流沙 <zhanglong@cai-inc.com>
2021-11-30 19:38:37 +0800

    编程题例题1题目

--- src/main/java/cn/zl/algo/week01/program/example/Example01.java
+++ src/main/java/cn/zl/algo/week01/program/example/Example01.java
@@ -1,5 +1,15 @@
 package cn.zl.algo.week01.program.example;
 
+/**
+ * 判断当前字符串是否是有效的IP地址
+ *
+ * 4段，由 . 分隔开
+ * 可以允许每个数字前后有空格；
+ * 数字内部不能有空格；
+ * 不能有非数字出现；
+ * 数字大小在[0,255]
+ */
 public class Example01 {
+
 
 }
