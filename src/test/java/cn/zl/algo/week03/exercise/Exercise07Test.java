349842b4b9b609e4348751fb4a715317c42e2a17
流沙 <zhanglong@cai-inc.com>
2021-12-15 17:53:18 +0800

    栈习题-简单计算器

--- src/test/java/cn/zl/algo/week03/exercise/Exercise07Test.java
+++ src/test/java/cn/zl/algo/week03/exercise/Exercise07Test.java
@@ -1,6 +1,5 @@
 package cn.zl.algo.week03.exercise;
 
-import com.sun.xml.internal.bind.v2.TODO;
 
 import java.util.Stack;
 
@@ -21,10 +20,63 @@ public class Exercise07Test {
         Stack<Integer> nums = new Stack<>();
         Stack<Character> ops = new Stack<>();
         while (i < n) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                i++;
+                continue;
+            }
+            if (isDigit(c)) {
+                int digit = 0;
+                while (i < n && isDigit(s.charAt(i))) {
+                    digit = digit * 10 + digit;
+                    i++;
+                }
+                nums.push(digit);
+                continue;
+            }
+
+            // 符号处理
+            while (!ops.isEmpty() && !prior(c, ops.peek())) {
+                fetchAndCalc(nums, ops);
+            }
+            ops.push(c);
 
         }
-        // TODO 没练完
-        return -1;
+
+        while (!ops.isEmpty()) {
+            fetchAndCalc(nums, ops);
+        }
+        return nums.pop();
+    }
+
+    private boolean prior(char a, char b) {
+        return (a == '*' || a == '/') && (b == '+' || b == '-');
+    }
+
+    private void fetchAndCalc(Stack<Integer> nums, Stack<Character> ops) {
+        int k2 = nums.pop();
+        int k1 = nums.pop();
+        int op = ops.pop();
+        int res = 0;
+        switch (op) {
+            case '+':
+                res = k1 + k2;
+                break;
+            case '-':
+                res = k1 - k2;
+                break;
+            case '*':
+                res = k1 * k2;
+                break;
+            case '/':
+                res = k1 / k2;
+                break;
+        }
+        nums.push(res);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
 }
