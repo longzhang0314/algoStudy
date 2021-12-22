9dbf2baa8d5ca566e8806c4a39d0a52a0675649a
流沙 <zhanglong@cai-inc.com>
2021-12-22 17:10:40 +0800

    额外

--- src/main/java/cn/zl/algo/week04/sort/addition/Addition01.java
+++ src/main/java/cn/zl/algo/week04/sort/addition/Addition01.java
@@ -1,29 +1,69 @@
 package cn.zl.algo.week04.sort.addition;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 1122. 数组的相对排序（简单）
- *
+ * <p>
  * 给你两个数组，arr1 和arr2，
- *
+ * <p>
  * arr2中的元素各不相同
  * arr2 中的每个元素都出现在arr1中
  * 对 arr1中的元素进行排序，使 arr1 中项的相对顺序和arr2中的相对顺序相同。未在arr2中出现过的元素需要按照升序放在arr1的末尾。
- *
+ * <p>
  * 输入：arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
  * 输出：[2,2,2,1,4,3,3,9,6,7,19]
- *
+ * <p>
  * 1 <= arr1.length, arr2.length <= 1000
  * 0 <= arr1[i], arr2[i] <= 1000
- * arr2中的元素arr2[i各不相同
- * arr2 中的每个元素arr2[i都出现在arr1中
- *
+ * arr2中的元素arr2[i]各不相同
+ * arr2 中的每个元素arr2[i]都出现在arr1中
+ * <p>
  * TODO
+ *
  * @author: longzhang
  * @date: 2021/12/21
  */
 public class Addition01 {
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public static void main(String[] args) {
+        Addition01 a = new Addition01();
+//        int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, arr2 = {2, 1, 4, 3, 9, 6};
 
+        int[] arr1 = {2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31};
+        int[] arr2 = {2,42,38,0,43,21};
+        int[] ints = a.relativeSortArray(arr1, arr2);
+        for (int b : ints) {
+            System.out.println(b);
+        }
     }
+
+    // 计数排序
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) return null;
+        Arrays.sort(arr1);
+        int[] res = new int[arr1.length];
+        int[] cnt = new int[1001];
+        int k = 0;
+        for (int a1 : arr1) {
+            cnt[a1]++;
+        }
+        for (int a2 : arr2) {
+            int c = cnt[a2];
+            for (int i = 0; i < c; i++) {
+                res[k++] = a2;
+            }
+            cnt[a2] = -1;
+        }
+        for (int a1 : arr1) {
+            if (cnt[a1] != -1) {
+                res[k++] = a1;
+            }
+        }
+        return res;
+    }
+
+
 }
--- src/main/java/cn/zl/algo/week04/sort/addition/Addition02.java
+++ src/main/java/cn/zl/algo/week04/sort/addition/Addition02.java
@@ -3,7 +3,7 @@ package cn.zl.algo.week04.sort.addition;
 /**
  * 493. 翻转对（困难）
  *
- * 给定一个数组nums，如果i < j且nums[i] > 2*nums[j我们就将(i, j)称作一个重要翻转对。
+ * 给定一个数组nums，如果i < j且nums[i] > 2*nums[j]我们就将(i, j)称作一个重要翻转对。
  *
  * 你需要返回给定数组中的重要翻转对的数量。
  *
@@ -16,15 +16,93 @@ package cn.zl.algo.week04.sort.addition;
  * 给定数组的长度不会超过50000。
  * 输入数组中的所有数字都在32位整数的表示范围内。
  *
- * TODO
- *
  * @author: longzhang
  * @date: 2021/12/21
  */
 public class Addition02 {
 
+    public static void main(String[] args) {
+        Addition02 a = new Addition02();
+        // p = 0, q = 2, r = 4
+//        int[] nums = {1,3,2,3,1};
+//        // 1,2,3 ,1,3
+//        int[] nums1 = {-5, -5};
+//        int[] nums2 = {2147483647,2147483647,-2147483647,-2147483647,-2147483647,2147483647};
+////        System.out.println(a.reversePairs(nums)); // 2
+////        System.out.println(a.reversePairs(nums1)); // 1
+//        System.out.println(a.reversePairs(nums2)); // 9
 
+
+    }
+
+
+    // 逆序对基础上判断2倍的关系
+    int cnt = 0;
     public int reversePairs(int[] nums) {
+        if (nums == null) return 0;
+        mergeSort(nums, 0, nums.length - 1);
+        return cnt;
+    }
 
+    private void mergeSort(int[] nums, int p, int r) {
+        if (p >= r) return;
+        int q = p + (r - p) / 2;
+        mergeSort(nums, p, q);
+        mergeSort(nums, q + 1, r);
+        merge(nums, p, q, r);
+    }
+
+    private void merge(int[] nums, int p, int q, int r) {
+        int[] tmp = new int[r - p + 1];
+        int i = p, j = q + 1, k = 0;
+
+        // 统计翻转对
+        calcReversePairs(nums, p, q, r);
+
+        while (i <= q && j <= r) {
+
+            if (nums[i] <= nums[j]) {
+                tmp[k++] = nums[i++];
+            } else {
+                tmp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= q) {
+            tmp[k++] = nums[i++];
+        }
+
+        while (j <= r) {
+            tmp[k++] = nums[j++];
+        }
+
+
+        for (i = 0; i <= r - p; i++) {
+            nums[p + i] = tmp[i];
+        }
+    }
+
+    private void calcReversePairs(int[] nums, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        while (i <= q && j <= r) {
+           if (isReversePairs(nums, i, j)) {
+                cnt += (q - i + 1);
+                j++;
+            } else {
+               i++;
+           }
+        }
+    }
+
+    private boolean isReversePairs(int[] nums, int i, int j) {
+        int a = nums[i];
+        int b = nums[j];
+        int border = Integer.MAX_VALUE / 2 + 1;
+        int border2 = Integer.MIN_VALUE / 2;
+        // 2 * b 越界处理
+        if (b >= border) return false;
+        if (b < border2) return true;
+        return a > b * 2;
     }
 }
