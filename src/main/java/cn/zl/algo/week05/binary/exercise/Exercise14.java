4b1a376ea225e64119ac9592b5c32c7b40952aa4
longzhang <dravenzl@163.com>
2022-01-09 18:44:20 +0800

    二分和哈希表复习

--- src/main/java/cn/zl/algo/week05/binary/exercise/Exercise14.java
+++ src/main/java/cn/zl/algo/week05/binary/exercise/Exercise14.java
@@ -23,7 +23,6 @@ import java.util.List;
  * 1 <= arr.length <= 104
  * 数组里的每个元素与 x 的绝对值不超过 104
  *
- * TODO【周末重新复习】 不够流畅
  *
  * @author liusha
  * @date 2021/12/28
@@ -32,16 +31,137 @@ public class Exercise14 {
 
     public static void main(String[] args) {
         Exercise14 e = new Exercise14();
-        int[] arr = {3,5,8,10};
-        int k = 2;
-        int x = 15;
+        int[] arr = {1,1,1,10,10,10};
+        int k = 1;
+        int x = 9;
         List<Integer> list = e.findClosestElements(arr, k, x);
         for (int num : list) {
             System.out.println(num);
         }
     }
 
+
+    // O(logN + k)
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        // 接近x顺序：
+        //（1） 相等：第一个等于x，然后向后遍历等于x的
+        // (2) 不相等：最后一个小于x的和第一个大于x的比较与x的绝对值
+
+        // 方法：先二分找到等于x的第一个索引、小于x的最后一个索引、大于x的第一个索引；
+        // 优先填充等于x的；填充完等于的开始填充小于x和大于x的，用双指针法填充
+
+        int n = arr.length;
+        int cnt = k;
+        List<Integer> res = new ArrayList<>(k);
+
+        // 不直接填充，只记录左右边界索引
+        int leftIdx = -1, rightIdx = -1;
+
+        // 第一个等于x的索引
+        int idx = findFirstEqu(arr, x);
+        leftIdx = idx;
+        while (k > 0 && idx >= 0 && idx < n && arr[idx] == x) {
+            rightIdx = idx++;
+            k--;
+        }
+        if (k == 0) {
+            addToRes(arr, leftIdx, rightIdx, res);
+            return res;
+        }
+
+        // 最后一个小于x的索引
+        int lastLessIdx = findLastLess(arr, x);
+        // 第一个大于x的索引
+        int firstMoreIdx = findFirstMore(arr, x);
+
+        while (k > 0 && (lastLessIdx >= 0 || firstMoreIdx < n)) {
+            // 左边的更接近，即lastLessIdx更接近
+            boolean isLeft = checkLeft(arr, x, lastLessIdx, firstMoreIdx);
+            if (isLeft) {
+                leftIdx = lastLessIdx--;
+            } else {
+                rightIdx = firstMoreIdx++;
+            }
+            k--;
+        }
+
+        // 如果没有相等的，且有一侧一直没命中，那么会有负一的情况
+        if (leftIdx == -1) {
+            leftIdx = rightIdx - cnt + 1;
+        } else if (rightIdx == -1) {
+            rightIdx = leftIdx + cnt - 1;
+        }
+
+        addToRes(arr, leftIdx, rightIdx, res);
+        return res;
+    }
+
+    private void addToRes(int[] arr, int left, int right, List<Integer> res) {
+        for (int i = left; i <= right; i++) {
+            res.add(arr[i]);
+        }
+    }
+
+    private boolean checkLeft(int[] arr, int x, int left, int right) {
+        int n = arr.length;
+        if (right >= n || right == -1) return true;
+        if (left < 0) return false;
+        int leftDiff = x - arr[left];
+        int rightDiff = arr[right] - x;
+        return leftDiff <= rightDiff;
+    }
+
+    private int findFirstMore(int[] arr, int x) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > x) {
+                if (mid == 0 || arr[mid - 1] <= x) return mid;
+                else right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int findLastLess(int[] arr, int x) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < x) {
+                if (mid == arr.length - 1 || arr[mid + 1] >= x) return mid;
+                else left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int findFirstEqu(int[] arr, int x) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == x) {
+                if (mid == 0 || arr[mid - 1] != x) return mid;
+                else right = mid - 1;
+            } else if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    // =======================================  初次尝试的方法 ==========================================================
+
+
+
+
+    public List<Integer> findClosestElements1(int[] arr, int k, int x) {
         if (arr == null || k <= 0 || k > arr.length) return new ArrayList<>();
         List<Integer> res = new ArrayList<>(k);
         int n = arr.length;
--- src/main/java/cn/zl/algo/week05/hash/exercise/Exercise16.java
+++ src/main/java/cn/zl/algo/week05/hash/exercise/Exercise16.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * 面试题 16.21. 交换和（中等） 找规律，可以借助哈希表优化
  *
- * TODO【重做】再练一遍
+ * 【注意】思路证明
  * @author liusha
  * @date 2022/1/2
  */
@@ -22,15 +22,21 @@ public class Exercise16 {
             sum2 += array2[i];
         }
         // 两个数元素交换后，一个数组和+a，另一个一定-a，说明交换前差值为2a
+        // 假设数组1更大，如果小，这里是负数
         int diff = sum1 - sum2;
+        // 差值为计数无法使相等
         if (diff % 2 != 0) return new int[0];
         diff /= 2;
+        // 一个数组+diff，一个数组-diff，我们假设数组1更大，所以数组1-diff,数组2+diff
+        // 把数组2的数全部存到set中，对于每个数组1的数，如果set存在num1-diff，那么两数交换
         Set<Integer> set = new HashSet<>();
-        for (int a2 : array2) {
-            set.add(a2);
+        for (int num2 : array2) {
+            set.add(num2);
         }
-        for (int a1 : array1) {
-            if (set.contains(a1 - diff)) return new int[]{a1, a1 - diff};
+        for (int num1 : array1) {
+            if (set.contains(num1 - diff)) {
+                return new int[]{num1, num1 - diff};
+            }
         }
         return new int[0];
     }
