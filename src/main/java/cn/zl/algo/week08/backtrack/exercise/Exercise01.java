5ba3e08edec5ca4d678d60dd63af1f7fd1b4694a
流沙 <zhanglong@cai-inc.com>
2022-01-24 14:37:08 +0800

    回溯前2题

---
+++ src/main/java/cn/zl/algo/week08/backtrack/exercise/Exercise01.java
@@ -0,0 +1,74 @@
+package cn.zl.algo.week08.backtrack.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 面试题 08.12 八皇后（困难）
+ *
+ * 用Q和.表示
+ * @author liusha
+ * @date 2022/1/24
+ */
+public class Exercise01 {
+
+    public List<List<String>> solveNQueens(int n) {
+        if (n <= 0) return new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
+        // 因为每行只能有1个皇后，所有用1维数组表示二维数据
+        int[] arr = new int[n];
+        // 从第
+        back(res, arr, 0, n);
+        return res;
+    }
+
+    private void back(List<List<String>> res, int[] arr, int row, int n) {
+        // 找到一种组合，返回，然后找别的可能性
+        if (row == n) {
+            List<String> list = arrToList(arr);
+            res.add(list);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isValid(arr, row, col)) {
+                // 当前合理就放入
+                arr[row] = col;
+                // 探索下一行放的元素
+                back(res, arr, row + 1, n);
+            }
+            // 全部不合理的话会弹栈，回到上一行重新选择，因为是数组赋值，所以不需要手动remove
+        }
+    }
+
+    private boolean isValid(int[] arr, int row, int col) {
+        int leftUp = col - 1, rightUp = col + 1;
+        int rowUp = row - 1;
+        while (rowUp >= 0) {
+            // 正上方，左上，右上是否有值
+            if (arr[rowUp] == col) return false;
+            if (leftUp >= 0 && arr[rowUp] == leftUp) return false;
+            if (rightUp < arr.length && arr[rowUp] == rightUp) return false;
+            rowUp--;
+            leftUp--;
+            rightUp++;
+        }
+        return true;
+    }
+
+    private List<String> arrToList(int[] arr) {
+        List<String> list =  new ArrayList<>(arr.length);
+        for (int row = 0; row < arr.length; row++) {
+            char[] chs = new char[arr.length];
+            int i = 0;
+            for(int col = 0; col < arr.length; col++) {
+                if (arr[row] == col) {
+                    chs[i++] = 'Q';
+                } else {
+                    chs[i++] = '.';
+                }
+            }
+            list.add(new String(chs));
+        }
+        return list;
+    }
+}
---
+++ src/main/java/cn/zl/algo/week08/backtrack/exercise/Exercise02.java
@@ -0,0 +1,74 @@
+package cn.zl.algo.week08.backtrack.exercise;
+
+/**
+ * 37.解数独
+ *
+ * @author liusha
+ * @date 2022/1/24
+ */
+public class Exercise02 {
+
+    public void solveSudoku(char[][] board) {
+        slove(board, 0, 0);
+    }
+
+    private boolean slove(char[][] board, int i, int j) {
+        if (j == 9) {
+            return slove(board, i + 1, 0);
+        }
+        if (i == 9) {
+            return true;
+        }
+
+        if (board[i][j] != '.') {
+            return slove(board, i, j + 1);
+        }
+
+        for (char k = '1'; k <= '9'; k++) {
+            if (isValid(board, i, j, k)) {
+                board[i][j] = k;
+                if (slove(board, i, j + 1)) {
+                    return true;
+                }
+                board[i][j] = '.';
+            }
+        }
+        return false;
+    }
+
+//    private boolean isValid(char[][] board, int row, int col, char c) {
+//        int n = board.length;
+//        for (int i = 0; i < n; i++) {
+//            if (board[row][i] == c) return false;
+//            if (board[i][col] == c) return false;
+//            if (board[(row / 3) * 3 + i / 3][(col / 3) * 3 + i % 3] == c) return false;
+//        }
+//        return true;
+//    }
+
+    private boolean isValid(char[][] board, int i, int j, char target) {
+        // 同一行
+        for (int m = 0; m < 9; m++) {
+            if (m == j) continue;
+            if (board[i][m] == target) return false;
+        }
+        // 同一列
+        for (int m = 0; m < 9; m++) {
+            if (m == i) continue;
+            if (board[m][j] == target) return false;
+        }
+
+        // 同一个3*3的框
+        int row = (i / 3) * 3;
+        int col = (j / 3) * 3;
+        for (int r = row; r < row + 3; r++) {
+            for (int c = col; c < col + 3; c++) {
+                if (i == r && j == c) continue;;
+                if (board[r][c] == target) return false;
+            }
+        }
+
+        return true;
+    }
+
+}
