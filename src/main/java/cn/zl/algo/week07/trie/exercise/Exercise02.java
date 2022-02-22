df3b8bc0c008c8a25ec135291756431aa18eb104
流沙 <zhanglong@cai-inc.com>
2022-02-22 16:07:40 +0800

    Trie树选做题

--- src/main/java/cn/zl/algo/week07/trie/exercise/Exercise02.java
+++ src/main/java/cn/zl/algo/week07/trie/exercise/Exercise02.java
@@ -1,11 +1,101 @@
 package cn.zl.algo.week07.trie.exercise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 面试题 17.17. 多次搜索（中等） 标准AC自动机，不过写AC自动机太复杂，Trie树搞定
  *
- * TODO do late Trie树选做题
  * @author: longzhang
  * @date: 2022/1/16
  */
 public class Exercise02 {
+
+
+    public static void main(String[] args) {
+        String big = "mississippi";
+        String[] smalls = {"is","ppi","hi","sis","i","ssippi"};
+        Exercise02 e = new Exercise02();
+        int[][] ints = e.multiSearch(big, smalls);
+        for (int[] i : ints) {
+            for (int j : i) {
+                System.out.print(j + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public int[][] multiSearch(String big, String[] smalls) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < smalls.length; i++) {
+            map.put(smalls[i], i);
+            insert(smalls[i].toCharArray());
+            res.add(new ArrayList<>());
+        }
+        char[] bigChs = big.toCharArray();
+        for (int i = 0; i < big.length(); i++) {
+            for (int j = i; j < big.length(); j++) {
+                boolean find = find(bigChs, i, j);
+                if (find) {
+                    String sub = big.substring(i, j + 1);
+                    int idx = map.get(sub);
+                    res.get(idx).add(i);
+                }
+            }
+        }
+        int[][] arr = new int[res.size()][];
+        for (int i = 0; i < res.size(); i++) {
+            arr[i] = new int[res.get(i).size()];
+            for (int j = 0; j < res.get(i).size(); j++) {
+                arr[i][j] = res.get(i).get(j);
+            }
+        }
+        return arr;
+    }
+
+    public boolean find(char[] chs, int i, int j) {
+        TrieNode p = root;
+        for (int k = i; k <= j; k++) {
+            char data = chs[k];
+            int idx = data - 'a';
+            if (p.children[idx] == null) return false;
+            p = p.children[idx];
+        }
+        return p.isEnd;
+    }
+
+
+    public void insert(char[] chs) {
+        TrieNode p = root;
+        for (int i = 0; i < chs.length; i++) {
+            char data = chs[i];
+            int idx = data - 'a';
+            if (p.children[idx] == null) {
+                p.children[idx] = new TrieNode(data);
+            }
+            p = p.children[idx];
+        }
+        p.isEnd = true;
+    }
+
+    private TrieNode root = new TrieNode('/');
+
+    private class TrieNode {
+        public char data;
+        public TrieNode[] children;
+        public boolean isEnd;
+
+        public TrieNode(char data) {
+            this.data = data;
+            this.children = new TrieNode[26];
+            this.isEnd = false;
+        }
+    }
+
+
+
 }
--- src/main/java/cn/zl/algo/week07/trie/exercise/Exercise03.java
+++ src/main/java/cn/zl/algo/week07/trie/exercise/Exercise03.java
@@ -1,10 +1,96 @@
 package cn.zl.algo.week07.trie.exercise;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * 212. 单词搜索 II（困难）
- * TODO do late Trie树选做题
+ *
  * @author: longzhang
  * @date: 2022/1/16
  */
 public class Exercise03 {
+
+    Set<String> set;
+    int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+    public List<String> findWords(char[][] board, String[] words) {
+        for (String word : words) {
+            insert(word.toCharArray());
+        }
+
+        set = new HashSet<>();
+        List<Character> path = new ArrayList<>();
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                path.add(board[i][j]);
+                visited[i][j] = true;
+                TrieNode p = root;
+                dfs(board, i, j, visited, path, p);
+                visited[i][j] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+        List<String> res = new ArrayList<>();
+        res.addAll(set);
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, boolean[][] visited, List<Character> path, TrieNode p) {
+        p = p.children[board[i][j] - 'a'];
+        if (p == null) return;
+        if (p.isEnd) {
+            char[] chs = new char[path.size()];
+            int k = 0;
+            for (char c : path) {
+                chs[k++] = c;
+            }
+            set.add(new String(chs));
+        }
+
+        for (int[] direction : directions) {
+            int row = i + direction[0];
+            int col = j + direction[1];
+            if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) continue;
+            if (visited[row][col]) continue;
+            visited[row][col] = true;
+            path.add(board[row][col]);
+            dfs(board, row, col, visited, path, p);
+            visited[row][col] = false;
+            path.remove(path.size() - 1);
+        }
+
+    }
+
+    public void insert(char[] chs) {
+        TrieNode p = root;
+        for (int i = 0; i < chs.length; i++) {
+            char data = chs[i];
+            int idx = data - 'a';
+            if (p.children[idx] == null) {
+                p.children[idx] = new TrieNode(data);
+            }
+            p = p.children[idx];
+        }
+        p.isEnd = true;
+    }
+
+    private TrieNode root = new TrieNode('/');
+
+    private class TrieNode {
+        public char data;
+        public TrieNode[] children;
+        public boolean isEnd;
+
+        public TrieNode(char data) {
+            this.data = data;
+            this.children = new TrieNode[26];
+            this.isEnd = false;
+        }
+    }
+
 }
