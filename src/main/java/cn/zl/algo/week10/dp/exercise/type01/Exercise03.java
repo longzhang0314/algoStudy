58b073d92a843e4db5c18d1bcea7918dae39a350
流沙 <zhanglong@cai-inc.com>
2022-03-02 11:44:40 +0800

    dp_week10_type1完成

--- src/main/java/cn/zl/algo/week10/dp/exercise/type01/Exercise03.java
+++ src/main/java/cn/zl/algo/week10/dp/exercise/type01/Exercise03.java
@@ -2,17 +2,43 @@ package cn.zl.algo.week10.dp.exercise.type01;
 
 /**
  * 322 零钱兑换
+ * 最少需要多少个凑成
  *
- * TODO do late 完全背包
+ * 完全背包
  * @author liusha
  * @date 2022/2/23
  */
 public class Exercise03 {
     public static void main(String[] args) {
         Exercise03 e = new Exercise03();
-        int[] coins = {1};
-        int amount = 0;
-        System.out.println(e.coinChange(coins, amount));
+        int[] coins = {1,2,5};
+        int amount = 11;
+        System.out.println(e.coinChange2(coins, amount));
+    }
+
+
+    public int coinChange2(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int k = 0; k <= amount / coins[0]; k++) {
+            dp[0][k * coins[0]] = k;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int cnt = j / coins[i];
+                for (int k = 0; k <= cnt; k++) {
+                    if (dp[i - 1][j - k * coins[i]] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - k * coins[i]] + k);
+                    }
+                }
+            }
+        }
+        return dp[n - 1][amount] == Integer.MAX_VALUE ? -1 : dp[n - 1][amount];
     }
 
 
--- src/main/java/cn/zl/algo/week10/dp/exercise/type01/Exercise04.java
+++ src/main/java/cn/zl/algo/week10/dp/exercise/type01/Exercise04.java
@@ -3,7 +3,9 @@ package cn.zl.algo.week10.dp.exercise.type01;
 /**
  * 518 零钱兑换2
  *
- * TODO do late 完全背包
+ * 有几种凑法
+ *
+ * 完全背包
  * @author liusha
  * @date 2022/2/23
  */
@@ -13,6 +15,23 @@ public class Exercise04 {
         int[] coins = {1,2,5};
         int amount = 5;
         System.out.println(e.change(amount, coins));
+    }
+
+    public int change2(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int k = 0; k <= amount / coins[0]; k++) {
+            dp[0][k * coins[0]] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int cnt = j / coins[i];
+                for (int k = 0; k <= cnt; k++) {
+                    dp[i][j] += dp[i - 1][j - k * coins[i]];
+                }
+            }
+        }
+        return dp[n - 1][amount];
     }
 
 
