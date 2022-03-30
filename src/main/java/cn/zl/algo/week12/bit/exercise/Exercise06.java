39e4934ec5c84bf88c8f176fffaffa22a38fc085
流沙 <zhanglong@cai-inc.com>
2022-03-30 20:26:34 +0800

    异或

--- src/main/java/cn/zl/algo/week12/bit/exercise/Exercise06.java
+++ src/main/java/cn/zl/algo/week12/bit/exercise/Exercise06.java
@@ -9,6 +9,28 @@ package cn.zl.algo.week12.bit.exercise;
  */
 public class Exercise06 {
 
+    // res = res ^ x ^ x
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        res ^= n;
+        return res;
+    }
+
+    // 等差数列：s = a1*n + [n*(n-1)*d] / 2; s =
+    public int missingNumber2(int[] nums) {
+        int n = nums.length;
+        int sum = (n + 1) * n / 2;
+        for (int i = 0; i < n; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+
 
 
 }
