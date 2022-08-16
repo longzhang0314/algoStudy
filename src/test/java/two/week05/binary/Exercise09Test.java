8f836fe7f4dd89feb62d1cd4d76305099dc164df
流沙 <zhanglong@cai-inc.com>
2022-08-16 20:23:21 +0800

    test02 week05 exercise09

--- src/test/java/two/week05/binary/Exercise09Test.java
+++ src/test/java/two/week05/binary/Exercise09Test.java
@@ -1,6 +1,5 @@
 package two.week05.binary;
 
-import com.sun.xml.internal.bind.v2.TODO;
 
 /**
  * @author liusha
@@ -8,6 +7,26 @@ import com.sun.xml.internal.bind.v2.TODO;
  */
 public class Exercise09Test {
 
+    public int peakIndexInMountainArray(int[] arr) {
+        if (arr == null || arr.length == 0) return -1;
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[(mid - 1 + arr.length) % arr.length]
+                    && arr[mid] > arr[(mid + 1) % arr.length]) {
+                return mid;
+            }
 
-    TODO
+            if (mid == 0) {
+                left = mid + 1;
+            } else if (mid == arr.length - 1) {
+                right = mid - 1;
+            } else if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
