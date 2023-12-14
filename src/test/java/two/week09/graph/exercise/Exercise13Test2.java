68612cfe5cebde106e3ec73eb4123f149739004f
流沙 <zhanglong@cai-inc.com>
2023-12-14 20:12:57 +0800

    week09 exercise12Test2 ing

---
+++ src/test/java/two/week09/graph/exercise/Exercise13Test2.java
@@ -0,0 +1,19 @@
+package two.week09.graph.exercise;
+
+import java.util.List;
+
+/**
+ * @author 流沙
+ * @date 2023/12/14
+ */
+public class Exercise13Test2 {
+
+    // 全小写英文字母
+    // 除begin外，都应该在wordlist中
+    // 最短长度，包含begin和end
+    // 无法到达，则返回0
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+
+    }
+}
