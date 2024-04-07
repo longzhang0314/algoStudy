b9144c0edc36eca254e27d883cf045ca8c55dd43
流沙 <zhanglong@cai-inc.com>
2024-04-07 14:45:33 +0800

    bug

--- src/test/java/two/week11/exercise/type03/Exercise01Test2.java
+++ src/test/java/two/week11/exercise/type03/Exercise01Test2.java
@@ -10,7 +10,7 @@ public class Exercise01Test2 {
 
     public static void main(String[] args) {
         Exercise01Test2 e = new Exercise01Test2();
-        int[] nums = {10,9,2,5,3,7,101,18};
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
         System.out.println(e.lengthOfLIS(nums)); // 4
     }
 
@@ -41,42 +41,40 @@ public class Exercise01Test2 {
     // 贪心：顺序遍历数组的情况下，同样长度的LIS，末尾元素越小越好
     // 二分：当前nums[i]
     public int lengthOfLIS2(int[] nums) {
-        public int lengthOfLIS(int[] nums) {
-            if (nums == null || nums.length == 0) return 0;
-            int n = nums.length;
-            // 贪心 + 二分：访问到i时，同样的LIS长度，末尾元素越小，越有利于全局的LIS长度
-            // dp数组定义：长度为i的LIS对应的最小末尾元素，方便处理边界情况定义为长度为i+1
-            int[] dp = new int[n];
-            dp[0] = nums[0];
-            // 全局LIS-1
-            int len = 0;
-            // 遍历到i时，对比nums[i]和dp[j<max]
-            //   如果nums[i]大于所有dp[j<max]，那么dp[max+1]=nums[i]，max++;
-            //   否则找到位置j，其中dp[j-1]<nums[i]<dp[j] => dp[j]=nums[i]
-            for (int i = 1; i < n; i++) {
-                if (nums[i] > dp[len]) {
-                    dp[++len] = nums[i];
-                } else {
-                    // 找到j，dp[j - 1] < nums[i] < dp[j]
-                    int left = 0, right = len;
-                    int j = -1;
-                    while (left <= right) {
-                        int mid = left + (right - left) / 2;
-                        if (dp[mid] <= nums[i]) {
-                            left = mid + 1;
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        // 贪心 + 二分：访问到i时，同样的LIS长度，末尾元素越小，越有利于全局的LIS长度
+        // dp数组定义：长度为i的LIS对应的最小末尾元素，方便处理边界情况定义为长度为i+1
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        // 全局LIS-1
+        int len = 0;
+        // 遍历到i时，对比nums[i]和dp[j<max]
+        //   如果nums[i]大于所有dp[j<max]，那么dp[max+1]=nums[i]，max++;
+        //   否则找到位置j，其中dp[j-1]<nums[i]<dp[j] => dp[j]=nums[i]
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > dp[len]) {
+                dp[++len] = nums[i];
+            } else {
+                // 找到j，dp[j - 1] < nums[i] < dp[j]
+                int left = 0, right = len;
+                int j = -1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (dp[mid] <= nums[i]) {
+                        left = mid + 1;
+                    } else {
+                        if (mid == 0 || dp[mid - 1] < nums[i]) {
+                            j = mid;
+                            break;
                         } else {
-                            if (mid == 0 || dp[mid - 1] < nums[i]) {
-                                j = mid;
-                                break;
-                            } else {
-                                right = mid - 1;
-                            }
+                            right = mid - 1;
                         }
                     }
-                    if (j != -1) dp[j] = nums[i];
                 }
+                if (j != -1) dp[j] = nums[i];
             }
-            return len + 1;
         }
+        return len + 1;
     }
 }
